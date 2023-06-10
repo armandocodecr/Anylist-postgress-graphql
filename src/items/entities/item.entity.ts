e3a482edc113cf0e16d3e0cce6db990557874728
ArmandoCode <totovip1464@gmail.com>
2023-06-09 19:55:26 -0600

    Nueva entity para el modulo 'item'

--- src/items/entities/item.entity.ts
+++ src/items/entities/item.entity.ts
@@ -1,7 +1,33 @@
-import { ObjectType, Field, Int } from '@nestjs/graphql';
+import { ObjectType, Field, ID } from '@nestjs/graphql';
+import { User } from 'src/users/entities/user.entity';
+import { Column, Entity, Index, ManyToOne, PrimaryGeneratedColumn } from 'typeorm';
 
+@Entity({ name: 'items' })
 @ObjectType()
 export class Item {
-  @Field(() => Int, { description: 'Example field (placeholder)' })
-  exampleField: number;
+  
+  @PrimaryGeneratedColumn('uuid')
+  @Field(() => ID)
+  id: string;
+
+  @Column()
+  @Field(() => String)
+  name: string;
+
+  // @Column()
+  // @Field(() => Float)
+  // quantity: number;
+
+  @Column({ nullable: true })
+  @Field(() => String, { nullable: true })
+  quantityUnits?: string; // g, ml, kg, tsp
+
+  // stores
+  // Relacion con los usuarios (En este caso es: Muchos items pertenecen a un mismo usuario)
+
+  @ManyToOne( () => User, (user) => user.items, { nullable: false, lazy: true } ) //Lazy permite que la BD cargue los datos de user
+  @Index('userId-Index')
+  @Field( ()=> User )
+  user: User;
+
 }
