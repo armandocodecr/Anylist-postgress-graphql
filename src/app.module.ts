1201770447f7083fc2b3f75ceb667e8dc7f0a66e
ArmandoCode <totovip1464@gmail.com>
2023-06-09 19:49:30 -0600

    app.module: Autenticacion por token

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,25 +1,51 @@
-import { ApolloDriverConfig, ApolloDriver } from '@nestjs/apollo';
-import { Module } from '@nestjs/common';
-import { GraphQLModule } from '@nestjs/graphql';
-import { ApolloServerPluginLandingPageLocalDefault } from '@apollo/server/plugin/landingPage/default';
 import { join } from 'path';
-import { ItemsModule } from './items/items.module';
+import { ApolloDriverConfig, ApolloDriver } from '@nestjs/apollo';
 import { ConfigModule } from '@nestjs/config';
+import { Module } from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
+
+import { GraphQLModule } from '@nestjs/graphql';
 import { TypeOrmModule } from '@nestjs/typeorm';
+
+import { ApolloServerPluginLandingPageLocalDefault } from '@apollo/server/plugin/landingPage/default';
+
+import { ItemsModule } from './items/items.module';
+import { UsersModule } from './users/users.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
   imports: [
 
     ConfigModule.forRoot(),
 
-    GraphQLModule.forRoot<ApolloDriverConfig>({
+    GraphQLModule.forRootAsync({
       driver: ApolloDriver,
-      playground: false,
-      plugins: [
-        ApolloServerPluginLandingPageLocalDefault()
-      ],
-      autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
-      }),
+      imports: [ AuthModule ],
+      inject: [ JwtService ],
+      useFactory: async(jwtService: JwtService ) => ({
+          playground: false,
+          plugins: [
+            ApolloServerPluginLandingPageLocalDefault()
+          ],
+          autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
+          context({ req }) { // Este código deniega a los querys de la BD a usuarios con token no autenticado
+            // const token = req.headers.authorization?.replace('Bearer ', '')
+            // if( !token ) throw new Error('Token needed')
+
+            // const payload = jwtService.decode( token )
+            // if( !payload ) throw new Error('Token not valid')
+          }
+        })
+    }),
+
+    // GraphQLModule.forRoot<ApolloDriverConfig>({
+    //   driver: ApolloDriver,
+    //   playground: false,
+    //   plugins: [
+    //     ApolloServerPluginLandingPageLocalDefault()
+    //   ],
+    //   autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
+    // }),
 
       TypeOrmModule.forRoot({
         type: 'postgres',
@@ -33,6 +59,10 @@ import { TypeOrmModule } from '@nestjs/typeorm';
       }),
       
     ItemsModule,
+      
+    UsersModule,
+      
+    AuthModule,
   ],
   controllers: [],
   providers: [],
