74e4bf2d5f182c81d13e4965e496bbb3858f7fa6
ArmandoCode <totovip1464@gmail.com>
2023-06-16 18:36:12 -0600

    Configuracion de la entidad de 'list-item'

--- src/list-item/entities/list-item.entity.ts
+++ src/list-item/entities/list-item.entity.ts
@@ -1,7 +1,33 @@
-import { ObjectType, Field, Int } from '@nestjs/graphql';
+import { ObjectType, Field, Int, ID } from '@nestjs/graphql';
+import { Column, Entity, ManyToOne, PrimaryGeneratedColumn, Unique } from 'typeorm';
 
+import { Item } from 'src/items/entities/item.entity';
+import { List } from 'src/lists/entities/list.entity';
+
+@Entity('listItems')
+@Unique('listItem-item', ['list','item'])
 @ObjectType()
 export class ListItem {
-  @Field(() => Int, { description: 'Example field (placeholder)' })
-  exampleField: number;
+  
+  @PrimaryGeneratedColumn('uuid')
+  @Field(() => ID)
+  id: string;
+
+  @Column({ type: 'numeric' })
+  @Field(() => Number)
+  quantity: number;
+
+  @Column({ type: 'boolean' })
+  @Field(() => Boolean)
+  completed: boolean
+
+  //Relaciones
+  @ManyToOne( () => List, (list)  => list.listItem, { lazy: true })
+  @Field(() => List)
+  list: List
+
+  @ManyToOne( () => Item, (item) => item.listItem, { lazy: true } )
+  @Field(() => Item)
+  item: Item
+
 }
